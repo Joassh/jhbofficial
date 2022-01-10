92103fb50bafe447079242ef9ff40e9086ca653b
Joassh <61348542+Joassh@users.noreply.github.com>
2022-01-11 01:09:16 +0530

    Add files via upload

---
+++ Movie-Review/reviewjs/reviewscript.js
@@ -0,0 +1,99 @@
+// Forwarding to next page
+document.getElementById("reviewcard1").href = "reviewcards/reviewcard1.html";
+document.getElementById("reviewcard2").href = "reviewcards/reviewcard2.html";
+document.getElementById("reviewcard3").href = "reviewcards/reviewcard3.html";
+document.getElementById("reviewcard4").href = "reviewcards/reviewcard4.html";
+document.getElementById("reviewcard5").href = "reviewcards/reviewcard5.html";
+document.getElementById("reviewcard6").href = "reviewcards/reviewcard6.html";
+document.getElementById("reviewcard7").href = "reviewcards/reviewcard7.html";
+document.getElementById("reviewcard8").href = "reviewcards/reviewcard8.html";
+document.getElementById("reviewcard9").href = "reviewcards/reviewcard9.html";
+document.getElementById("reviewcard10").href = "reviewcards/reviewcard10.html";
+document.getElementById("reviewcard11").href = "reviewcards/reviewcard11.html";
+document.getElementById("reviewcard12").href = "reviewcards/reviewcard12.html";
+document.getElementById("reviewcard13").href = "reviewcards/reviewcard13.html";
+// --------------------------------------------------------------------------------------------------------------------------
+// Handling Cards
+//Heading
+document.getElementById("heading-card1").innerHTML = "Spider-Man:No Way Home";
+document.getElementById("heading-card2").innerHTML = " Pushpa: The Rise - Part 1";
+document.getElementById("heading-card3").innerHTML = "Jail";
+document.getElementById("heading-card4").innerHTML = "Bachelor";
+document.getElementById("heading-card5").innerHTML = "Irudhi Pakkam";
+document.getElementById("heading-card6").innerHTML = "Anti Indian";
+document.getElementById("heading-card7").innerHTML = "Thalli Pogathey";
+document.getElementById("heading-card8").innerHTML = "Anandham Vilayadum Veedu";
+document.getElementById("heading-card9").innerHTML = "Writer";
+document.getElementById("heading-card10").innerHTML = "83";
+document.getElementById("heading-card11").innerHTML = "Minal Murali ";
+document.getElementById("heading-card12").innerHTML = "Atrangi Re - Galata Kalyanam";
+document.getElementById("heading-card13").innerHTML = "Velan";
+
+document.getElementById("image-card1").src = "reviewimages/noway.jpg";
+document.getElementById("image-card2").src = "reviewimages/pushpa.webp";
+document.getElementById("image-card3").src = "reviewimages/Jail.jpg";
+document.getElementById("image-card4").src = "reviewimages/Bachelor.jpeg";
+document.getElementById("image-card5").src = "reviewimages/irudhipakkam.jpg";
+document.getElementById("image-card6").src = "reviewimages/antiindian.jpg";
+document.getElementById("image-card7").src = "reviewimages/thali.jpg";
+document.getElementById("image-card8").src = "reviewimages/anantham.jpg";
+document.getElementById("image-card9").src = "reviewimages/writer.jpg";
+document.getElementById("image-card10").src = "reviewimages/83.jpg";
+document.getElementById("image-card11").src = "reviewimages/minalmurali.jpg";
+document.getElementById("image-card12").src = "reviewimages/Atrangi.jpg";
+document.getElementById("image-card13").src = "reviewimages/velan.jpeg";
+
+document.getElementById("castcrew-1").innerHTML =
+  "Tom Holland, Benedict Cumberbatch, Zendaya, Marisa Tomei, Jon Favreau, Alfred Molina, Jamie Foxx, Willem Dafoe";
+document.getElementById("castcrew-2").innerHTML =
+  "Allu Arjun, Rashmika Mandanna, Fahadh Faasil, Rao Ramesh, Anasuya Bharadwaj, Ajay Ghosh";
+document.getElementById("castcrew-3").innerHTML =
+  "G. V. Prakash Kumar, Abarnathi, Radikaa Sarathkumar, Pakoda Pandi";
+document.getElementById("castcrew-4").innerHTML =
+  "G. V. Prakash Kumar, Divya Bharathi, Bagavathi Perumal, Munishkanth";
+document.getElementById("castcrew-5").innerHTML =
+  "Amrutha Srinivasan, Rajesh Balachandiran, Vignesh Shanmugam,...";
+document.getElementById("castcrew-6").innerHTML =
+  "Elamaran, Radha Ravi, Aadukalam Naren, Velu Prabhakaran";
+document.getElementById("castcrew-7").innerHTML =
+  "Anupama Parameswaran, Samuthirakani, Atharvaa Murali, Amitash Pradhan";
+document.getElementById("castcrew-8").innerHTML =
+  "Gautham Karthik, Shivathmika Rajashekar, Cheran, Saravanan, Vignesh, Mottai Rajendran, Soundara Raja";
+document.getElementById("castcrew-9").innerHTML =
+  "P. Samuthirakani, Hari Krishnan";
+document.getElementById("castcrew-10").innerHTML =
+  "Ranveer Singh, Deepika Padukone, Ammy Virk, Jiiva, Harrdy Sandhu, Sahil Khattar, Chirag Patil, R Badree, Jatin Sarna, Dhairya Karwa, Saqib Saleem, Tahir Raj Bhasin, Adinath Kothare, Pankaj Tripathi, Dinker Sharma,Boman Irani";
+document.getElementById("castcrew-11").innerHTML =
+  "Tovino Thomas, Jude Anthany Joseph, Basil Joseph, Guru Somasundaram, P. Balachandran, Aju Varghese, Harisree Asokan, Vasisht Umesh, Femina George, Shelly Kishore";
+document.getElementById("castcrew-12").innerHTML =
+  "Akshay Kumar, Dhanush, Sara Ali Khan, Menka Rai, Florian Dibra, Heroxicated Armaan";
+document.getElementById("castcrew-13").innerHTML =
+  "Mugen Rao, Meenakshi Govindarajan, Soori, Prabhu";
+
+document.getElementById("lang-1").innerHTML = "English, Hindi, Tamil, Telugu";
+document.getElementById("lang-2").innerHTML = " Telugu, Tamil, Hindi, Malayalam, Kannada";
+document.getElementById("lang-3").innerHTML = "Tamil";
+document.getElementById("lang-4").innerHTML = "Tamil";
+document.getElementById("lang-5").innerHTML = "Tamil";
+document.getElementById("lang-6").innerHTML = "Tamil";
+document.getElementById("lang-7").innerHTML = "Tamil";
+document.getElementById("lang-8").innerHTML = "Tamil";
+document.getElementById("lang-9").innerHTML = "Tamil";
+document.getElementById("lang-10").innerHTML = "Hindi, Tamil, Telugu, Kannada, Malayalam";
+document.getElementById("lang-11").innerHTML = "Malayalam ";
+document.getElementById("lang-12").innerHTML = "Hindi";
+document.getElementById("lang-13").innerHTML = "Tamil";
+
+document.getElementById("date-1").innerHTML = " 16 Dec 2021 | 2 hrs 28 mins";
+document.getElementById("date-2").innerHTML = "17 Dec 2021 | 2 hrs 59 mins";
+document.getElementById("date-3").innerHTML = "09 Dec 2021 | 2 hrs 14 mins";
+document.getElementById("date-4").innerHTML = "03 Dec 2021 | 2 hrs 45 mins";
+document.getElementById("date-5").innerHTML = "17 Dec 2021 | 1 hr 35 mins";
+document.getElementById("date-6").innerHTML = "2 hr 4 mins";
+document.getElementById("date-7").innerHTML = "24 Dec 2021 | 2 hrs 6 mins";
+document.getElementById("date-8").innerHTML = " 24 Dec 2021 | 2 hrs 27 mins";
+document.getElementById("date-9").innerHTML = " 24 Dec 2021 | 2 hrs 30 mins";
+document.getElementById("date-10").innerHTML = " 24 Dec 2021 | 1 hrs 52 mins";
+document.getElementById("date-11").innerHTML = " 24 Dec 2021 | 2 hrs 38 mins";
+document.getElementById("date-12").innerHTML = " 24 Dec 2021";
+document.getElementById("date-13").innerHTML = "31 Dec 2021 | 2 hrs 14 mins";
